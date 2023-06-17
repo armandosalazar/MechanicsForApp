fc9f8f25329d404335528fb64796020760e2df69
Eduardo Robles <lozanoeduardo8777@gmail.com>
2023-06-17 13:27:18 -0600

    Add a file for item of RecyclerView and fix some problems on Menu

---
+++ app/src/main/java/me/armandosalazar/mechanicsforapp/recyclerview/Item.java
@@ -0,0 +1,32 @@
+package me.armandosalazar.mechanicsforapp.recyclerview;
+
+public class Item {
+
+    private String name;
+    private String typeOfMechanic;
+    private int image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeOfMechanic() {
+        return typeOfMechanic;
+    }
+
+    public void setTypeOfMechanic(String typeOfMechanic) {
+        this.typeOfMechanic = typeOfMechanic;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
