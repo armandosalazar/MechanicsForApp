0de4bb8bcc8f23b0f505caf843a3152fa0f5aee2
Armando Salazar <armandosalazar351@gmail.com>
2023-06-17 20:11:26 -0600

    reciclyview

--- app/src/main/java/me/armandosalazar/mechanicsforapp/recyclerview/CustomAdapter.java
+++ app/src/main/java/me/armandosalazar/mechanicsforapp/recyclerview/CustomAdapter.java
@@ -1,4 +1,63 @@
 package me.armandosalazar.mechanicsforapp.recyclerview;
 
-public class CustomAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import me.armandosalazar.mechanicsforapp.R;
+import me.armandosalazar.mechanicsforapp.models.Mechanic;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+    private final ArrayList<Mechanic> mechanics;
+
+    public CustomAdapter(ArrayList<Mechanic> mechanics) {
+        this.mechanics = mechanics;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView mechanicName;
+        private final TextView mechanicType;
+
+        public ViewHolder(@NonNull ViewGroup parent) {
+            super(parent);
+            mechanicName = parent.findViewById(R.id.mechanic_name);
+            mechanicType = parent.findViewById(R.id.mechanic_type);
+            parent.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    System.out.println("Position: " + position);
+                    System.out.println("Mechanic: " + mechanics.get(position).getName());
+                }
+            });
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public CustomAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.mechanic_item, parent, false);
+        return new ViewHolder((ViewGroup) view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomAdapter.ViewHolder holder, int position) {
+        Mechanic mechanic = mechanics.get(position);
+        holder.mechanicName.setText(mechanic.getName());
+        holder.mechanicType.setText(mechanic.getTypeOfMechanic());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mechanics.size();
+    }
+
 }
--- app/src/main/java/me/armandosalazar/mechanicsforapp/ui/home/HomeFragment.java
+++ app/src/main/java/me/armandosalazar/mechanicsforapp/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package me.armandosalazar.mechanicsforapp.ui.home;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,23 +10,55 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import me.armandosalazar.mechanicsforapp.dao.DAO;
 import me.armandosalazar.mechanicsforapp.databinding.FragmentHomeBinding;
+import me.armandosalazar.mechanicsforapp.models.Mechanic;
+import me.armandosalazar.mechanicsforapp.recyclerview.CustomAdapter;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    // recycler view
+    private RecyclerView recyclerView;
+    private CustomAdapter customAdapter;
+    private ArrayList<Mechanic> mechanics;
+
+    // shared preferences
+    private SharedPreferences sharedPreferences;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         // final TextView textView = binding.textHome;
         // homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        sharedPreferences = getActivity().getSharedPreferences("mechanics.dat", 0);
+        mechanics = DAO.getInstance(sharedPreferences).getMechanics();
+
+        if (mechanics == null) {
+            mechanics = new ArrayList<>();
+        }
+
+        for (Mechanic mechanic : mechanics) {
+            System.out.println(mechanic.getName());
+        }
+
+        // recyclerView = binding.recyclerMechanics;
+        recyclerView = root.findViewById(binding.recyclerMechanics.getId());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        customAdapter = new CustomAdapter(mechanics);
+        recyclerView.setAdapter(customAdapter);
+
         return root;
     }
 
